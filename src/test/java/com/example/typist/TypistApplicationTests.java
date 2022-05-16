da54adf699df40e93e254a4bcae11fe170a58bfc
vitaliiPsl <vitalii.psl.main@gmail.com>
2022-05-16 13:49:05 +0300

    Init commit

---
+++ src/test/java/com/example/typist/TypistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.typist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
