f5e72ab8543f2e9b6aee3c5e86eaa17b2577c92d
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-10 16:30:09 +0200

    Update JwtTokenVerifierFilter to use token exchange from AuthService

--- src/main/java/com/example/typist/config/security/jwt/JwtTokenVerifierFilter.java
+++ src/main/java/com/example/typist/config/security/jwt/JwtTokenVerifierFilter.java
@@ -1,8 +1,13 @@
 package com.example.typist.config.security.jwt;
 
-import io.jsonwebtoken.JwtException;
+import com.example.typist.payload.error.ApiError;
+import com.example.typist.service.AuthService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -14,26 +19,50 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
+@RequiredArgsConstructor
 @Component
 public class JwtTokenVerifierFilter extends OncePerRequestFilter {
+    private final AuthService authService;
+    private final ObjectMapper objectMapper;
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
+        String authorization = req.getHeader(HttpHeaders.AUTHORIZATION);
 
         if (authorization == null || authorization.isBlank() || !authorization.startsWith("Bearer ")) {
-            filterChain.doFilter(request, response);
+            filterChain.doFilter(req, res);
             return;
         }
 
+        verifyToken(req, res, filterChain, authorization);
+    }
+
+    private void verifyToken(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain, String authorization) throws IOException {
+        log.debug("Verify authorization: {}", authorization);
+
         String token = authorization.replace("Bearer ", "");
-
         try {
-            Authentication authentication = JwtUtils.tokenToAuthenticationMapper(token);
+            Authentication authentication = authService.exchangeToken(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
-        } catch (JwtException e) {
-            throw new BadCredentialsException("Token cannot be trusted", e);
-        }
 
-        filterChain.doFilter(request, response);
+            filterChain.doFilter(req, res);
+        } catch (Exception e) {
+            log.error("Jwt token failed verification: {}", token, e);
+            writeErrorResponse(res);
+        }
+    }
+
+    private void writeErrorResponse(HttpServletResponse response) throws IOException {
+        ApiError apiError = new ApiError(HttpStatus.UNAUTHORIZED, "Invalid token");
+
+        String responseBody = objectMapper.writeValueAsString(apiError);
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setContentLength(responseBody.length());
+
+        response.getWriter().print(responseBody);
+        response.getWriter().flush();
     }
 }
