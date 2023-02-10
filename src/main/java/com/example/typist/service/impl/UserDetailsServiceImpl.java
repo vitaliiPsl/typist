17cefed309238697138cce02ed414b12a1eb4238
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-10 11:12:25 +0200

    Implement UserDetailsService

---
+++ src/main/java/com/example/typist/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.typist.service.impl;
+
+import com.example.typist.model.User;
+import com.example.typist.repository.UserRepository;
+import com.example.typist.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Supplier;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    private final String NOT_FOUND_MESSAGE = "User with username %s not found";
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(exceptionSupplier(username));
+    }
+
+    private Supplier<UsernameNotFoundException> exceptionSupplier(String username) {
+        return () -> new UsernameNotFoundException(String.format(NOT_FOUND_MESSAGE, username));
+    }
+}
