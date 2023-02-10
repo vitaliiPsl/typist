69d51db63e573ae42f25eee3eedd1c1dfa501c04
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-10 12:02:45 +0200

    Add signin endpoint

--- src/main/java/com/example/typist/controller/AuthController.java
+++ src/main/java/com/example/typist/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.example.typist.controller;
 
 import com.example.typist.payload.UserDto;
+import com.example.typist.payload.auth.SignInRequest;
+import com.example.typist.payload.auth.SignInResponse;
 import com.example.typist.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,5 +21,10 @@ public class AuthController {
     @PostMapping("/signup")
     public UserDto signup(@RequestBody @Valid UserDto userDto) {
         return authService.signUp(userDto);
+    }
+
+    @PostMapping("/signin")
+    SignInResponse signIn(@Valid @RequestBody SignInRequest request) {
+        return authService.signIn(request);
     }
 }
