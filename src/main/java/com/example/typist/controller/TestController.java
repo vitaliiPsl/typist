7fadd9a63cea9abbe6189398db2a9b3a1a88acd4
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-12 16:07:13 +0200

    Move TestController to controller package and update endpoints

---
+++ src/main/java/com/example/typist/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.example.typist.controller;
+
+import com.example.typist.model.User;
+import com.example.typist.payload.TestDto;
+import com.example.typist.service.TestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@CrossOrigin
+@RestController
+@RequestMapping("api/tests")
+public class TestController {
+    private final TestService testService;
+
+    @PostMapping
+    public TestDto saveTest(@Valid @RequestBody TestDto testDto, @AuthenticationPrincipal User actor) {
+        return testService.saveTest(testDto, actor);
+    }
+}
