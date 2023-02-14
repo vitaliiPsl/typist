7c75a3c498f65eb71dae1d961be2a66f73e2038b
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-14 17:43:39 +0200

    Replace TestResult with functional TestREsult and ResultScore

---
+++ frontend/src/components/test/test-result/TestResult.jsx
@@ -0,0 +1,17 @@
+import ResultScore from './ResultScore'
+
+const TestResult = ({ result }) => {
+	return (
+		<div className='test-result w-full flex justify-between gap-4'>
+            <ResultScore score={result.wpm.toFixed(1)} label={'wpm'} />
+
+            <ResultScore score={result.rawWpm.toFixed(1)} label={'raw wpm'} />
+
+            <ResultScore score={result.accuracy.toFixed(2)} label={'accuracy'} />
+            
+            <ResultScore score={result.duration.toFixed(0)} label={'duration'} />
+		</div>
+	)
+}
+
+export default TestResult
