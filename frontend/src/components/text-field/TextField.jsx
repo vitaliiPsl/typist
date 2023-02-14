2417bb1fbcb4e9d8f8b1250eb043fcef1cb9e558
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-14 17:49:29 +0200

    Some tweaks in Button adn TextField

--- frontend/src/components/text-field/TextField.jsx
+++ frontend/src/components/text-field/TextField.jsx
@@ -1,27 +1,29 @@
 const TextField = ({
 	name,
-	type = 'text',
-	placeholder,
 	onChange,
 	reference,
+	placeholder,
+	type = 'text',
 	required = false,
+	disabled = false,
 	autoComplete = 'off',
 	className = '',
 	inputClassName = '',
 }) => {
 	return (
 		<div
-			className={`text-field ${className} w-full flex justify-center gap-6`}
+			className={`text-field ${className} min-w-0 w-full flex justify-center gap-6`}
 		>
 			<input
 				name={name}
 				type={type}
-				placeholder={placeholder}
+				ref={reference}
 				onChange={onChange}
 				required={required}
+				disabled={disabled}
+				placeholder={placeholder}
 				autoComplete={autoComplete}
-				ref={reference}
-				className={`field ${inputClassName} min-h-11 w-full py-2 px-4 text-base text-txPrimary bg-bgPrimary border-2 border-txPrimary rounded-lg duration-500 focus:outline-none focus:text-bgPrimary focus:bg-txPrimary`}
+				className={`field ${inputClassName} min-h-11 w-full py-2 px-4 text-base text-txPrimary bg-bgPrimary border-2 border-txPrimary rounded-lg duration-500 focus:outline-none focus:text-bgPrimary focus:bg-txPrimary disabled:bg-bgSecondary`}
 			/>
 		</div>
 	)
