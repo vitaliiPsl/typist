1ab6857f1f26cb5183753ef25372a944aca64277
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-14 13:00:19 +0200

    Create testConfigSlice

---
+++ frontend/src/app/features/test/testConfigSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+	duration: 30,
+	durationOptions: [15, 30, 60, 90],
+}
+
+export const testConfigSlice = createSlice({
+	name: 'test-config',
+	initialState,
+	reducers: {
+		setDuration: (state, action) => {
+			let duration = action.payload
+			state.duration = duration
+		},
+        setDurationOptions: (state, action) => {
+            let options = action.payload
+            state.durationOptions = options
+        }
+	},
+})
+
+export const { setDuration } = testConfigSlice.actions
+
+export default testConfigSlice.reducer
