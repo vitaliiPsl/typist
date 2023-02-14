16edacf5f844c252bf3365dd0b2e930b72e84818
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-14 17:44:00 +0200

    Replace TestConfiguration with TestConfig

---
+++ frontend/src/components/test/TestConfig.jsx
@@ -0,0 +1,40 @@
+import { useDispatch, useSelector } from 'react-redux'
+
+import { setDuration } from '../../app/features/test/testConfigSlice'
+
+const TestConfig = ({className}) => {
+	const { duration, durationOptions } = useSelector(
+		(state) => state.testConfig
+	)
+
+	const dispatch = useDispatch()
+
+	const handleDurationSelection = (duration) => {
+		dispatch(setDuration(duration))
+	}
+
+	const mapOptions = (options) => {
+		return options.map((option, index) => (
+			<span
+				className={`config-option cursor-pointer ${
+					option === duration && 'text-highlight'
+				}`}
+				onClick={() => handleDurationSelection(option)}
+				key={index}
+			>
+				{option}
+			</span>
+		))
+	}
+
+	return (
+		<div className={`test-config ${className}`}>
+			<div className='config-row duration flex gap-2'>
+				<span>time: </span>
+				{mapOptions(durationOptions)}
+			</div>
+		</div>
+	)
+}
+
+export default TestConfig
