59ab489963eb025a36407b0b5ef7874fc19444f7
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-14 13:01:01 +0200

    Register text and test config reducers in redux store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,11 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import authReducer from './features/auth/authSlice'
+import textReducer from './features/text/textSlice'
+import testConfigReducer from './features/test/testConfigSlice'
+
 import { apiSlice } from './api'
 
 export const store = configureStore({
 	reducer: {
 		auth: authReducer,
+        text: textReducer,
+		testConfig: testConfigReducer,
 		[apiSlice.reducerPath]: apiSlice.reducer,
 	},
 	middleware: (getDefaultMiddleware) =>
