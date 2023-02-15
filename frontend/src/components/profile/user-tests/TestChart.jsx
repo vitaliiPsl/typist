6e645192d0b5f496d9cd90a98f423f8f58f0dd4e
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-15 19:08:03 +0200

    Replace TestChart with functional component

---
+++ frontend/src/components/profile/user-tests/TestChart.jsx
@@ -0,0 +1,66 @@
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend,
+} from 'chart.js'
+
+import { Line } from 'react-chartjs-2'
+
+ChartJS.register(
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend
+)
+
+const TestsChart = ({ tests }) => {
+	const options = {
+		responsive: true,
+		color: '#fff',
+		scales: {
+			color: '#d8d8d8',
+			yAxes: {
+				ticks: {
+					color: '#d8d8d8',
+				},
+			},
+			xAxes: {
+				ticks: {
+					color: '#d8d8d8',
+				},
+			},
+		},
+	}
+
+	const mapTests = (tests) => {
+		return {
+			labels: tests.map((test) => ''),
+			datasets: [
+				{
+					label: 'WPM',
+					data: tests.map((test) => test.wpm),
+					borderColor: ['#df972c'],
+					backgroundColor: ['#df972c'],
+				},
+			],
+		}
+	}
+
+	return (
+		<div className='user-tests-chart'>
+			<Line data={mapTests(tests)} options={options} />
+		</div>
+	)
+}
+
+export default TestsChart
