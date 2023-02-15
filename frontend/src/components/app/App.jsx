323d0940256f0d6d1150a83d0dadb0451fc83681
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-15 20:53:01 +0200

    Implement load of the authenticated user

--- frontend/src/components/app/App.jsx
+++ frontend/src/components/app/App.jsx
@@ -1,30 +1,63 @@
+import { useEffect } from 'react'
+
+import { useSelector, useDispatch } from 'react-redux'
+import { setUser } from '../../app/features/auth/authSlice'
+import { useGetAuthenticatedUserQuery } from '../../app/features/auth/authApi'
+
 import { Route, Routes } from 'react-router-dom'
 
+import ProtectedRoute from '../routes/ProtectedRoute'
 import Header from '../header/Header'
+
+import Test from '../test/Test'
 
 import SignInForm from '../auth/SignInForm'
 import SignUpForm from '../auth/SignUpForm'
 
-import Profile from '../profile/Profile'
+import UserProfile from '../profile/UserProfile'
 import Settings from '../settings/Settings'
 
 import Error404 from '../errorPages/Error404'
 import Error500 from '../errorPages/Error500'
 
-import Test from '../test/Test'
-
 const App = () => {
-	
+	const { user, token } = useSelector((state) => state.auth)
+
+	const { data, error, isLoading } = useGetAuthenticatedUserQuery(null, {
+		skip: !token,
+	})
+
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		if (data) {
+			dispatch(setUser(data))
+		}
+		if (error) {
+			handleUserError(error)
+		}
+	}, [data, error])
+
+	const handleUserError = (error) => {
+		console.log(error)
+	}
+
 	return (
 		<div className='App min-h-screen min-w-full flex flex-col text-txPrimary bg-bgPrimary overflow-hidden'>
-			<div className='container min-w-full px-40 flex-1 flex flex-col gap-6'>
-				<Header />
+			<div className='container min-w-full px-60 flex-1 flex flex-col gap-6'>
+				<Header user={user} />
 
 				<Routes>
 					<Route path={'/'} element={<Test />} />
 
-					<Route path={'/settings'} element={<Settings />} />
-					<Route path={'/profile/:id'} element={<Profile />} />
+					<Route element={<ProtectedRoute token={token} />}>
+						<Route path={'/settings'} element={<Settings />} />
+					</Route>
+
+					<Route
+						path={'/profile/:userId'}
+						element={<UserProfile />}
+					/>
 
 					<Route path={'/auth'}>
 						<Route path={'signin'} element={<SignInForm />} />
