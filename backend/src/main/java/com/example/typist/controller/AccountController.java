9652341546c12c54162e77524c94531631c4ac9f
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-16 20:49:57 +0200

    Add endpoint for changing nickname

--- backend/src/main/java/com/example/typist/controller/AccountController.java
+++ backend/src/main/java/com/example/typist/controller/AccountController.java
@@ -2,12 +2,13 @@ package com.example.typist.controller;
 
 import com.example.typist.model.User;
 import com.example.typist.payload.UserDto;
+import com.example.typist.payload.account.ChangeNicknameRequest;
 import com.example.typist.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,5 +19,10 @@ public class AccountController {
     @GetMapping
     public UserDto getAuthenticatedUser(@AuthenticationPrincipal User actor) {
         return accountService.getAuthenticatedUser(actor);
+    }
+
+    @PutMapping("/nickname")
+    public UserDto updateNickname(@RequestBody @Valid ChangeNicknameRequest request, @AuthenticationPrincipal User actor){
+        return accountService.changeNickname(request, actor);
     }
 }
