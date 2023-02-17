feea313de29f8a4c551fd4e95f1d11e4ce6eca15
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-17 19:47:19 +0200

    Create functional Error component

---
+++ frontend/src/components/errors/Error.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react'
+import MaterialIcon from '../material-icon/MaterialIcon'
+
+const Error = ({ message, onClose }) => {
+	// close error after 15 seconds
+	useEffect(() => {
+		setInterval(() => onClose(), 1000 * 15)
+	})
+
+	return (
+		<div className='error min-w-40 py-2 px-6 pr-2 flex items-center justify-between gap-4 absolute bottom-4 right-4 border border-red-600 rounded-lg z-50'>
+			<div className='error-message-box flex-1'>
+				<span className='error-message'>{message}</span>
+			</div>
+
+			<MaterialIcon icon={'close'} onClick={onClose} />
+		</div>
+	)
+}
+
+export default Error
