ff61816f21b6419f72f6f31cb617b9a07beb8ca6
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-18 19:50:43 +0200

    Replace previous image implementation with Image component

--- frontend/src/components/profile/UserDetails.jsx
+++ frontend/src/components/profile/UserDetails.jsx
@@ -1,16 +1,19 @@
+import Image from '../image/Image'
+
+import { API_URL } from '../../config'
+
 const UserDetails = ({ user }) => {
 	return (
 		<div className='profile-user-details py-4 flex items-center justify-around gap-6 border-b border-b-txSecondary'>
-			<div className='profile-user-avatar-box w-14 h-14 flex-shrink-0 flex items-center justify-center bg-zinc-800 text-white border-2 border-white rounded-full overflow-hidden'>
-				{user.avatar && (
-					<img src={user.avatar} alt='' className='user-avatar' />
-				)}
-				{!user.avatar && (
-					<span className='material-symbols-outlined user-avatar'>
-						person
-					</span>
-				)}
+			<div className='image-wrapper w-16 h-16'>
+				<Image
+					image={
+						user.image ? `${API_URL}/images/${user.image}` : null
+					}
+					icon={'person'}
+				/>
 			</div>
+
 			<div className='profile-user-nickname-box'>
 				<span className='profile-user-nickname text-2xl'>
 					{user.nickname}
