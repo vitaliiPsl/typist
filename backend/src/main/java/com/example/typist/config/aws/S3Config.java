6cde454223de2c795aa69d78718a599a9d680256
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-18 17:21:50 +0200

    Configure S3 client

---
+++ backend/src/main/java/com/example/typist/config/aws/S3Config.java
@@ -0,0 +1,29 @@
+package com.example.typist.config.aws;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    @ConditionalOnProperty("aws.s3.access-key")
+    @Bean
+    S3Client s3Client(
+            @Value("${aws.s3.access-key}") String accessKey,
+            @Value("${aws.s3.secret-key}") String secretKey,
+            @Value("${aws.s3.region}") String region
+    ) {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+        return S3Client.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .region(Region.of(region))
+                .build();
+    }
+}
