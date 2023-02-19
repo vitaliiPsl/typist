78cf70992f151b3efaea2e2b39301b5be30290dd
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-19 20:05:28 +0200

    Create endpoint for fetching tests

--- frontend/src/app/features/test/testApi.js
+++ frontend/src/app/features/test/testApi.js
@@ -10,9 +10,26 @@ export const testApi = apiSlice.injectEndpoints({
 			}),
 		}),
 		getTestByUserId: builder.query({
-			query: (userId) => `/tests?userId=${userId}`,
+			query: (userId) => `/tests?userId=${userId}&sortBy=${'timestamp'}`,
+		}),
+		getTestsRanking: builder.query({
+			query: (args) => ({
+				url: `/tests?limit=${10}&direction=${'DESC'}`,
+			}),
+		}),
+		getTests: builder.query({
+			query: (args) => ({
+				url: `/tests`,
+				params: args,
+			}),
 		}),
 	}),
 })
 
-export const { useSaveTestMutation, useGetTestByUserIdQuery } = testApi
+export const {
+	useSaveTestMutation,
+    useGetTestsQuery,
+    useLazyGetTestsQuery,
+	useGetTestByUserIdQuery,
+	useLazyGetTestsRankingQuery,
+} = testApi
