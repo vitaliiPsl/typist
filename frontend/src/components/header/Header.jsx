7ec5167bb27cefe1dccc47bcfc8fc43d99628da7
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-20 18:53:06 +0200

    Build burger menu

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -4,15 +4,24 @@ import { clear } from '../../app/features/auth/authSlice'
 import { NavLink, useNavigate } from 'react-router-dom'
 
 import NavigationLink from './NavigationLink'
+import Image from '../image/Image'
 import MaterialIcon from '../material-icon/MaterialIcon'
+import { useState } from 'react'
 
 const Header = ({ user }) => {
+	const [menuOpen, setMenuOpen] = useState(false)
+
 	const dispatch = useDispatch()
 	const navigate = useNavigate()
 
 	const logout = () => {
 		dispatch(clear())
+		closeMenu()
 		navigate('/')
+	}
+
+	const closeMenu = () => {
+		setMenuOpen(false)
 	}
 
 	return (
@@ -25,8 +34,10 @@ const Header = ({ user }) => {
 				</NavLink>
 			</div>
 
-			<div className={'header-nav-box flex items-center gap-6'}>
-				<NavigationLink to={'/'} label={'Home'} />
+			<div
+				className={'header-nav-box hidden lg:flex items-center gap-6 '}
+			>
+				<NavigationLink to={'/'} label={'Test'} />
 
 				{!user && (
 					<>
@@ -43,6 +54,66 @@ const Header = ({ user }) => {
 							label={user.nickname}
 						/>
 						<MaterialIcon icon={'logout'} onClick={logout} />
+					</>
+				)}
+			</div>
+
+			<div className='header-nav-burger-menu lg:hidden z-20'>
+				<Image
+					rounded='rounded-md'
+					className={'hover:border-0'}
+					icon={menuOpen ? 'close' : 'menu'}
+					onClick={() => setMenuOpen((menuOpen) => !menuOpen)}
+				/>
+			</div>
+
+			<div
+				className={`header-nav-menu-box fixed top-0 right-0 z-10 h-full w-full py-10 px-20 flex flex-col items-stretch justify-center gap-8 bg-[#000d] ${
+					menuOpen ? '-translate-y-0' : '-translate-y-full'
+				} ease-in duration-300`}
+			>
+				<NavigationLink
+					to={'/'}
+					label={'Test'}
+					onClick={closeMenu}
+					className={'border border-txPrimary rounded-lg'}
+				/>
+
+				{!user && (
+					<>
+						<NavigationLink
+							to={'/auth/signin'}
+							label={'Sign in'}
+							className={'border border-txPrimary rounded-lg'}
+							onClick={closeMenu}
+						/>
+						<NavigationLink
+							to={'/auth/signup'}
+							label={'Sign up'}
+							className={'border border-txPrimary rounded-lg'}
+							onClick={closeMenu}
+						/>
+					</>
+				)}
+
+				{user && (
+					<>
+						<NavigationLink
+							to={'/settings'}
+							label={'Settings'}
+                            className={'border border-txPrimary rounded-lg'}
+							onClick={closeMenu}
+						/>
+
+						<div className='profile-nav-link w-full flex gap-2'>
+							<NavigationLink
+								to={`/profile/${user.id}`}
+								label={user.nickname}
+                                className={'border border-txPrimary rounded-lg flex-1'}
+								onClick={closeMenu}
+							/>
+							<MaterialIcon icon={'logout'} onClick={logout} />
+						</div>
 					</>
 				)}
 			</div>
--- frontend/src/components/header/NavigationLink.jsx
+++ frontend/src/components/header/NavigationLink.jsx
@@ -1,14 +1,14 @@
 import { NavLink } from 'react-router-dom'
 
-const NavigationLink = ({ label, to }) => {
+const NavigationLink = ({ label, to, className, onClick }) => {
 	return (
-		<div className='navigation-link min-w-30 text-center'>
+		<div className={`navigation-link min-w-30 ${className}`} onClick={onClick}>
 			<NavLink
 				to={to}
 				className={({ isActive }) =>
-					isActive
-						? 'py-1 px-2 rounded-md duration-150 text-center bg-highlight'
-						: 'py-1 px-2 rounded-md duration-150 text-center'
+					`w-full h-full py-2 px-2 flex items-center justify-center rounded-md duration-150 text-center ${
+						isActive ? 'bg-highlight' : ''
+					}`
 				}
 			>
 				<span className='font-medium'>{label}</span>
