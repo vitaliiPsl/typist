a24a7b96c7428261e1300dda8a285138366547d1
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-23 11:03:27 +0200

    Create request payload for resending email confirmation token

---
+++ backend/src/main/java/com/example/typist/payload/auth/ResendEmailTokenRequest.java
@@ -0,0 +1,19 @@
+package com.example.typist.payload.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResendEmailTokenRequest {
+    @Email(message = "Email address must be valid")
+    @NotBlank(message = "Email address is required")
+    private String email;
+}
